db3481764de6cf04f082621a80ef36926be2bca8
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-01-17 03:57:48 -0300

    Adiciona funcionalidade de manter conexao

--- assets/js/api/uolApi.js
+++ assets/js/api/uolApi.js
@@ -6,4 +6,11 @@ apiMethods.enterChat = (participant) =>{
     return axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , {name: participant})
 }
 
+apiMethods.persistUser = () =>{
+    setInterval(() =>{
+        axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: userLoggedInformation.name })
+    }, 5000)
+}
+
+
 export { apiMethods }
